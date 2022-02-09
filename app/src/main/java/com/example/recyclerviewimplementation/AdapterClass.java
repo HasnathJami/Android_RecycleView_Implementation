d2bc134c9138b664f2a51a113c27c715b7c61e0a
jishan <62906638+HasnathJami@users.noreply.github.com>
2022-02-10 05:20:28 +0600

    Fill Uploaded

---
+++ app/src/main/java/com/example/recyclerviewimplementation/AdapterClass.java
@@ -0,0 +1,68 @@
+package com.example.recyclerviewimplementation;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class AdapterClass  extends RecyclerView.Adapter<AdapterClass.viewHolder> {
+
+
+
+    private Context context;
+    private ArrayList<Model> list;
+
+    public AdapterClass(Context context, ArrayList<Model> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    //1. Attach the sample_layout template to the backend and return this to viewHolder class by passing value to the constructor
+    @NonNull
+    @Override
+    public viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view= LayoutInflater.from(context).inflate(R.layout.sample_layout,parent,false);
+        return new viewHolder(view);
+    }
+
+    //3 first of all get the position of first item from the list and set it to the model;
+    //then setText to the textViews. Since data's are coming from api through model so call data and fetch data from model.
+    @Override
+    public void onBindViewHolder(@NonNull viewHolder holder, int position) {
+
+        Model model=list.get(position);
+        holder.tv1.setText(model.getUserId().toString());
+        holder.tv2.setText(model.getId());
+        holder.tv3.setText(model.getTitle());
+        holder.tv4.setText(model.getBody());
+
+        //Glide.with(context).load(model.getPoster()).into(holder.imageViewId);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    //2 Receive the value of view from onCreate ViewHolder and fetch all the id's from frontend  for the further works.
+    public class viewHolder extends RecyclerView.ViewHolder{
+
+        TextView tv1,tv2,tv3,tv4;
+        public viewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            tv1=itemView.findViewById(R.id.textView1Id);
+            tv2=itemView.findViewById(R.id.textView2Id);
+            tv3=itemView.findViewById(R.id.textView3Id);
+            tv4=itemView.findViewById(R.id.textView4Id);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/recyclerviewimplementation/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.recyclerviewimplementation;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.recyclerviewimplementation.databinding.ActivityMainBinding;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    ArrayList<Model> list;
+    RequestQueue requestQueue;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        //setContentView(R.layout.activity_main);
+
+        binding.recyclerViewId.setHasFixedSize(true);
+        binding.recyclerViewId.setLayoutManager(new LinearLayoutManager(this));
+
+        requestQueue = Volley.newRequestQueue(this);
+        list=new ArrayList<>();
+        fetchData();
+
+
+
+    }
+
+    private  void fetchData() {
+         String url="https://jsonplaceholder.typicode.com/posts";
+         JsonArrayRequest jsonArrayRequest=new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+             @Override
+             public void onResponse(JSONArray response) {
+
+                 JSONObject obj;
+                 String st1,st2,st3,st4;
+                 for(int i=0;i<response.length();i++)
+                 {
+                     try {
+                         obj=response.getJSONObject(i);
+                         st1=obj.getString("userId");
+                         st2=obj.getString("id");
+                         st3=obj.getString("title");
+                         st4=obj.getString("body");
+
+                         Model model= new Model(st1,st2,st3,st4);
+                         list.add(model);
+
+
+
+                     } catch (JSONException e) {
+                         e.printStackTrace();
+                     }
+
+                     AdapterClass adapterClass=new AdapterClass(MainActivity.this,list);
+                     binding.recyclerViewId.setAdapter(adapterClass);
+                 }
+
+             }
+         }, new Response.ErrorListener() {
+             @Override
+             public void onErrorResponse(VolleyError error) {
+                 //Toast.makeText(MainActivity.this, error.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+             }
+         });
+         requestQueue.add(jsonArrayRequest);
+    }
+}
